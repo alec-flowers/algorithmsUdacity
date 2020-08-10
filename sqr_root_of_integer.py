3722b475bd2dbc662bd9ef4b7e13b738766e6d03
Alec Flowers <alec.flowers00@gmail.com>
2020-08-10 23:36:45 +0200

    Problem 1

--- sqr_root_of_integer.py
+++ sqr_root_of_integer.py
@@ -11,19 +11,21 @@ def sqrt(number):
     """
     if number <= 0:
         return 0
+    if number <= 1:
+        return 1
 
     x = number
     while int(x) != int(.5 * (x + number/x)):
+        print(x)
         x = .5 * (x + number/x)
 
     return int(x)
 
+if __name__ == "__main__":
+    print('Calculated: {}  Expected: {}'.format(sqrt(25),5))
+    print('Calculated: {}  Expected: {}'.format(sqrt(100),10))
+    print('Calculated: {}  Expected: {}'.format(sqrt(3599),59))
+    print('Calculated: {}  Expected: {}'.format(sqrt(0),0))
+    print('Calculated: {}  Expected: {}'.format(sqrt(1),1))
+    print('Calculated: {}  Expected: {}'.format(sqrt(928342),963))
 
-print(sqrt(100))
-
-
-print ("Pass" if  (3 == sqrt(9)) else "Fail")
-print ("Pass" if  (0 == sqrt(0)) else "Fail")
-print ("Pass" if  (4 == sqrt(16)) else "Fail")
-print ("Pass" if  (1 == sqrt(1)) else "Fail")
-print ("Pass" if  (5 == sqrt(27)) else "Fail")
